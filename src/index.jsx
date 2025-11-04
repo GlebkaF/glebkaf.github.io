24918c4607b8668e55adb84d0c20a5f447244186
Glebka Fokin <GlebkaF@users.noreply.github.com>
2025-11-04 17:06:48 +0700

    Add resume layout for homepage

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,16 @@
 import { render } from "preact";
 import { LocationProvider, Router, Route } from "preact-iso";
 
-import { Header } from "./components/Header.jsx";
-import { Home } from "./pages/Home/index.jsx";
 import { NotFound } from "./pages/_404.jsx";
 import "./style.css";
-import { Movies } from "./pages/Movies/index.jsx";
+import { Resume } from "./pages/Resume/index.jsx";
 
 export function App() {
   return (
     <LocationProvider>
-      <Header />
       <main>
         <Router>
-          <Route path="/" component={Movies} />
+          <Route path="/" component={Resume} />
           <Route default component={NotFound} />
         </Router>
       </main>
